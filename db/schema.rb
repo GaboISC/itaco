6c29d5a435e6b1c24d733fe70347b08bdbc04739
GaboISC <11460258@itcolima.edu.mx>
2015-11-06 08:56:22 -0600

    Fix add product

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151106013359) do
+ActiveRecord::Schema.define(version: 20151106085057) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -47,7 +47,9 @@ ActiveRecord::Schema.define(version: 20151106013359) do
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
 
   create_table "products", force: :cascade do |t|
-    t.string   "Name"
+    t.string   "name"
+    t.integer  "price"
+    t.integer  "stock"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

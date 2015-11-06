17c64a9e42c252764f39a0807ffce579a6e5cf42
GaboISC <11460258@itcolima.edu.mx>
2015-11-06 02:14:43 -0600

    Add Welcome

--- db/migrate/20151106013359_create_products.rb
+++ db/migrate/20151106013359_create_products.rb
@@ -2,7 +2,9 @@ class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
       t.string :Name
-
+      t.string :Price
+      t.string :Stock
+      
       t.timestamps null: false
     end
   end
